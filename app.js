2a90592500d47f99b33a0daa18c09d1fd56cb725
Dominik Schöni <dominik@dominikschoeni.ch>
2015-04-02 11:05:53 +0200

    fixes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 'use strict';
 
 // os utils
-var os  = require('os-utils');
+var os = require('os-utils');
 var _ = require('lodash');
 
 // redis
@@ -20,16 +20,16 @@ var url = 'mongodb://localhost:27017/myproject';
 var collection;
 
 // Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
     console.log("Connected correctly to server");
     collection = db.collection('sentiment');
 
-    channel.on('error', function(error) {
+    channel.on('error', function (error) {
         console.log('Stopping due to: ' + error);
         process.exit();
     });
 
-    channel.connect(function() {
+    channel.connect(function () {
         console.log('ready');
         consumeTweet(); // enter consume-loop
     });
@@ -41,47 +41,47 @@ var tweetsCount = 0;
 var tweetsTotalSentiment = 0;
 
 // Worker funktion
-var consumeTweet = function() {
+var consumeTweet = function () {
 
     channel.pop(queueName, function (tweet) {
 
         var phrases = tweet.phrase.split(",");
-        var identifiedPhrase = null;
+        var tweetText = tweet.text + tweet.username + tweet.hashtag;
 
-        _.forEach(phrases, function(phrase) {
-            if (tweet.text.match(phrase) != null) {
-                identifiedPhrase = phrase;
+        _.forEach(phrases, function (phrase) {
+            if (tweetText.search(phrase) !== -1) {
+
+                Sentiment(tweet.text, function (err, result) {
+                    tweetsTotalSentiment += result.score;
+                    tweetsCount++;
+
+                    if (tweetsCount % 15 === 0) {
+                        var calculatedSentiment = tweetsTotalSentiment / 15;
+                        tweetsTotalSentiment = 0;
+
+                        os.cpuUsage(function (v) {
+                            console.log('CPU Usage (%): ' + v);
+                        });
+
+                        collection.insertOne({
+                            phrase: phrase,
+                            sentiment: calculatedSentiment
+                        }, function (err, result) {
+                            console.log("Inserted Tweets");
+                        });
+
+                        consumeTweet(); // and consume moar!
+
+                    } else {
+                        consumeTweet(); // consume moar!
+                    }
+
+                });
+
+            } else {
+                console.log("Skipped Tweet.");
             }
         });
-
-        if (identifiedPhrase != null) {
-
-            Sentiment(tweet.text, function (err, result) {
-                tweetsTotalSentiment += result.score;
-                tweetsCount++;
-
-                if (tweetsCount % 15 === 0) {
-                    var calculatedSentiment = tweetsTotalSentiment / 15;
-                    tweetsTotalSentiment = 0;
-
-                    os.cpuUsage(function (v) {
-                        console.log('CPU Usage (%): ' + v);
-                    });
-
-                    collection.insertOne({
-                        phrase: identifiedPhrase,
-                        sentiment: calculatedSentiment
-                    }, function (err, result) {
-                        console.log("Inserted Tweets");
-                    });
-
-                    consumeTweet(); // and consume moar!
-                } else {
-                    consumeTweet(); // consume moar!
-                }
-            });
-
-        }
 
     });
 }
