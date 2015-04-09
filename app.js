3ccd126f4d5db6255831026cb3e6c89161f19138
Clabfabs <islerfab@gmail.com>
2015-04-09 17:05:02 +0200

    put the tests for matching outside the consumeTweet() function to save memory, added the async-package to run the for-each-phrase loop asynchronously (should in my opinion generate more load)

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 var os = require('os-utils');
 var _ = require('lodash');
 var mongoose = require('mongoose');
-
+var async = require('async');
 
 // redis
 var queueName = "tweetQueue";
@@ -55,7 +55,7 @@ var consumeTweet = function () {
 
         totalTweets++;
 
-        if (totalTweets % 15 == 0) {
+        if (totalTweets % 50 == 0) {
             os.cpuUsage(function (v) {
                 console.log('CPU Usage (%): ' + v);
             });
@@ -63,70 +63,13 @@ var consumeTweet = function () {
 
         var phrases = tweet.phrase.split(",");
         var tweet_text = tweet.text;
-        var urls = tweet.entities.urls; // [] -> expanded_url, display_url
-        var media = tweet.entities.media; // [] -> expanded_url, display_url
-        var hashtags = tweet.entities.hashtags; // [] -> text
-        var user_mentions = tweet.entities.user_mentions; // [] -> screen_name
 
-        var match = function (phrase) {
-
-            var match = false;
-
-            var twitterMatch = function (text) {
-                // TODO: to be improved with fancy regex according to https://dev.twitter.com/streaming/overview/request-parameters#track
-                return text.search(phrase) > -1;
-            };
-
-            // Check Tweet text
-            if (twitterMatch(tweet_text)) {
-                // console.log("Matched " + phrase + " in text: " + tweet_text);
-                match = true;
-            }
-
-            // check urls
-            _.forEach(urls, function (url) {
-                if (url.hasOwnProperty('expanded_url') && twitterMatch(url.expanded_url)
-                    || url.hasOwnProperty('display_url') && twitterMatch(url.display_url)) {
-                    // console.log("Matched " + phrase + " in URL:");
-                    // console.log(url);
-                    match = true;
-                }
+        async.each(phrases, function (phrase, done) {
+            var matched = _.find(tests, function (testfunction) {
+                return testfunction(tweet, phrase);
             });
 
-            // check media
-            _.forEach(media, function (medium) {
-                if (medium.hasOwnProperty('expanded_url') && twitterMatch(medium.expanded_url)
-                    || medium.hasOwnProperty('display_url') && twitterMatch(medium.display_url)) {
-                    // console.log("Matched " + phrase + " in media:");
-                    // console.log(medium);
-                    match = true;
-                }
-            });
-
-            // check hashtags
-            _.forEach(hashtags, function (ht) {
-                if (ht.hasOwnProperty('text') && twitterMatch(ht.text)) {
-                    // console.log("Matched " + phrase + " in hashtag: " + ht.text);
-                    match = true;
-                }
-            });
-
-            // check user mentions
-            _.forEach(user_mentions, function (user) {
-                if (user.hasOwnProperty('screen_name') && twitterMatch(user.screen_name)) {
-                    //console.log("Matched " + phrase + " in user mentions: " + user.screen_name);
-                    match = true;
-                }
-            });
-
-            return match;
-        };
-
-        _.forEach(phrases, function (phrase) {
-            var matched = match(phrase);
-            // console.log(matched);
             if (matched) {
-
                 Sentiment(tweet_text, function (err, result) {
                     if (err) throw err;
 
@@ -138,32 +81,66 @@ var consumeTweet = function () {
 
                     newKeyword.save(function (err, keyword) {
                         if (err) throw err;
-                        // console.log("Inserted new phrase");
-                        consumeTweet();
                     });
                 });
-            } else {
-                consumeTweet();
             }
+            done();
+        }, function () {
+            consumeTweet();
         });
     });
 };
 
-var aggregate = function() {
+var aggregate = function () {
     var o = {};
-    o.map = function() { emit(this.keyword, this.sentiment) };
-    o.reduce = function(k, vals) {
+    o.map = function () {
+        emit(this.keyword, this.sentiment)
+    };
+    o.reduce = function (k, vals) {
         var sum = 0;
         for (var i = 0; i < vals.length; i++) {
             sum += vals[i];
         }
         return sum / vals.length;
     };
-    o.out = { replace: 'aggregated'};
+    o.out = {replace: 'aggregated'};
     o.verbose = true;
 
-    Keyword.mapReduce(o, function(err, model, stats) {
+    Keyword.mapReduce(o, function (err, model, stats) {
         if (err) throw err;
         console.log('map reduce finished in ' + stats.processtime + ' ms. check collection aggregated!');
     })
 };
+
+function twitterMatch(text, phrase) {
+    // TODO: to be improved with fancy regex according to https://dev.twitter.com/streaming/overview/request-parameters#track
+    return text.search(phrase) > -1;
+}
+
+var tests = [
+    function testText(tweet, phrase) {
+        return twitterMatch(tweet.text, phrase)
+    },
+    function testUrls(tweet, phrase) {
+        return _.find(tweet.entities.urls, function (url) {
+            return url.hasOwnProperty('expanded_url') && twitterMatch(url.expanded_url, phrase)
+                || url.hasOwnProperty('display_url') && twitterMatch(url.display_url, phrase);
+        });
+    },
+    function testMedia(tweet, phrase) {
+        return _.find(tweet.entities.media, function (medium) {
+            return medium.hasOwnProperty('expanded_url') && twitterMatch(medium.expanded_url, phrase)
+                || medium.hasOwnProperty('display_url') && twitterMatch(medium.display_url, phrase);
+        });
+    },
+    function testHashtags(tweet, phrase) {
+        return _.find(tweet.entities.hashtags, function (hashtag) {
+            return hashtag.hasOwnProperty('text') && twitterMatch(hashtag.text, phrase)
+        });
+    },
+    function testUserMentions(tweet, phrase) {
+        return _.find(tweet.entities.user_mentions, function (user) {
+            return user.hasOwnProperty('screen_name') && twitterMatch(user.screen_name, phrase)
+        });
+    }
+];
